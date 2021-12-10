2ac0ae161077599653f97431cd70aa891c326c18
Subham Das <57589556+subhamdas-github@users.noreply.github.com>
2021-12-10 17:24:31 +0530

    Update py-telnet.py

--- py-telnet.py
+++ py-telnet.py
@@ -1,8 +1,8 @@
 import getpass
 import telnetlib
 
-HOST = "localhost" # Ip address of the router
-user = input("Enter your telnet username: ")
+HOST = "<your machine ip>" # windows - ipconfig  / linux - ifconfig/ip addr
+user = input("Enter your telnet username: ") # telnet username created by you
 password = getpass.getpass()
 
 tn = telnetlib.Telnet(HOST)
@@ -13,6 +13,7 @@ if password:
     tn.read_until(b"Password: ")
     tn.write(password.encode('ascii') + b"\n")
 
+# router config commands we pass to the function
 tn.write(b"enable\n")
 tn.write(b"cisco\n")
 tn.write(b"conf t\n")
